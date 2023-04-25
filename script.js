a4568c9e7dc2477c5ab1455bc9febe4b4f3501f2
MKHLink <likhonhasankomol@gmail.com>
2023-04-25 15:51:20 -0400

    Final version, everything working as intended

--- script.js
+++ script.js
@@ -2,45 +2,45 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
 
-  // Navigate to the login page
+  // Navigates to the page
   await page.goto('https://idp.bootcampspot.com/ui/?requestId=a754904e-4a40-415a-8f87-7d53ee21db27');
 
-  // Enter your username and password
-  await page.type('#emailAddress', 'your-username');
-  await page.type('#passwordLabel', 'your-password');
+//  If required to log in, selects the fields via thei id and then input credentials next to them
+  await page.type('#emailAddress', '');
+  await page.type('#passwordLabel', '');
 
-  // Click the login button
+//   // Click the login button
   await page.click('#loginSubmit');
 
-  // Wait for the page to load after logging in
   await page.waitForNavigation();
 
-  // Navigate to the initial page with the first button
-  await page.goto('https://courses.bootcampspot.com/courses/1991/pages/1-dot-1-1-introduction?module_item_id=700942');
+// After logging in, if required to go to a spcific page, paste link here, otherwise remove
+  await page.goto('');
 
   let continueLooping = true;
-  let counter = 0;
+  let counter = 700;
 
-  const saveLocation = 'something';
+  // put save location as relative pathing
+  const saveLocation = '';
 
   while (continueLooping) {
 
-    let fileName = 'screenshot.png'+counter;
+    let fileName = 'screenshot'+counter+'.pdf';
     let filePath = `${saveLocation}/${fileName}`;
-    // Take a screenshot of the page
-    await page.screenshot({ path: filePath });
 
-    // Click the button
+    // Take a screenshot of the page and save it at the location as a pdf
+    await page.pdf({ path: filePath, format: 'A4', printBackground
+  : true});
+
+    //This logic clicks the next button on the page, selected by ID followed by the a element
     try {
       await page.click('span.module-sequence-footer-button--next a');
 
-      // Wait for the page to load after clicking the button
       await page.waitForNavigation();
 
-      // Wait for the next button to appear, with a timeout of 5 seconds
       await page.waitForSelector('span.module-sequence-footer-button--next', { timeout: 5000 });
 
       // If the next button does not appear within 5 seconds, we have reached the end of the sequence
@@ -49,9 +49,9 @@ const fs = require('fs');
       continueLooping = false;
     }
 
+    // incrementing counter so screenshots have unique names
     counter++;
   }
 
-  // Close the browser
   await browser.close();
 })();
